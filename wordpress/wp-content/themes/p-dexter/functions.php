e5e70c0160bdd1545ae7e37098705cae44fd5c51
emwing <emwing@emily-wing.com>
2018-05-06 18:41:21 -0400

    stupid jquery no work!

--- wordpress/wp-content/themes/p-dexter/functions.php
+++ wordpress/wp-content/themes/p-dexter/functions.php
@@ -92,7 +92,7 @@
 
 // add js
   function my_theme_scripts() {
-    wp_enqueue_script( 'main', get_template_directory_uri() . '/js/main.js', array( 'jquery' ), '1.0.0', true );
+    wp_enqueue_script( 'main', get_template_directory_uri() . '/js/main.js', array( 'jquery' ), array('jquery'), null, true );
   }
   add_action( 'wp_enqueue_scripts', 'my_theme_scripts' );
 /////////////////////////////
--- wordpress/wp-content/themes/p-dexter/js/main.js
+++ wordpress/wp-content/themes/p-dexter/js/main.js
@@ -1,26 +1,17 @@
-jQuery(document).ready(function() {
+$(document).ready(function() {
 
   // filter pokedex based on pokemon type
-  jQuery('.typeBox').off().on('click', function() {
-    var $name = jQuery(this).attr("name");
-    var $post = jQuery('.pokemon');
+  // $('#bugCB').click(function() {
+  //   var $name = $(this).attr("name");
+  //   var $post = $('.pokemon');
 
-    if( jQuery(this).is(':checked')) {
+    // if( $(this).is(':checked')) {
       alert('yay');
       // $("#ProjectListButton").show();
-    } else {
-      alert('boo');
+    // } else {
+      // alert('boo');
       // $("#ProjectListButton").hide();
-    }
-  });
-  // $('input[type="checkbox"]').change(function () {
-  //   var $name = $(this).attr("name");
-  //   var $typeBox = $("#waterCB");
-  //   if $typeBox.is(':checked')) {
-  //     console.log('yay');
-  //   } else {
-  //     console.log('boo');
-  //   }
-  // })
+    // }
+  // });
 
 } ); // ends document ready
